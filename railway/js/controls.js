9f781728ccbf384ee56507ea5d1d2d640cd970e4
AJJackGIS <kafei.aojie2012@foxmail.com>
2018-11-06 16:30:04 +0800

    block other railway

--- railway/js/controls.js
+++ railway/js/controls.js
@@ -1,13 +1,13 @@
 function addControl() {
     // 加载控件
     jg_group = L.layerGroup([railway_jg, mileStone_jg, station_jg, trouble_jg]);
-    hy_group = L.layerGroup([railway_hy, mileStone_hy, station_hy, trouble_hy]);
-    hw_group = L.layerGroup([railway_hw, mileStone_hw, station_hw, trouble_hw]);
-    yw_group = L.layerGroup([railway_yw, mileStone_yw, station_yw, trouble_yw]);
-    wj_group = L.layerGroup([railway_wj, mileStone_wj]);
-    wg_group = L.layerGroup([railway_wg, mileStone_wg]);
-    wx_group = L.layerGroup([railway_wx, mileStone_wx]);
-    wxx_group = L.layerGroup([railway_wxx, mileStone_wxx]);
+    // hy_group = L.layerGroup([railway_hy, mileStone_hy, station_hy, trouble_hy]);
+    // hw_group = L.layerGroup([railway_hw, mileStone_hw, station_hw, trouble_hw]);
+    // yw_group = L.layerGroup([railway_yw, mileStone_yw, station_yw, trouble_yw]);
+    // wj_group = L.layerGroup([railway_wj, mileStone_wj]);
+    // wg_group = L.layerGroup([railway_wg, mileStone_wg]);
+    // wx_group = L.layerGroup([railway_wx, mileStone_wx]);
+    // wxx_group = L.layerGroup([railway_wxx, mileStone_wxx]);
 
     jg_group.addTo(map); // 默认添加京广线
 
@@ -18,14 +18,14 @@ function addControl() {
     };
     var overlays = {
         "京广高铁": jg_group,
-        "汉宜高铁": hy_group,
-        "合武高铁": hw_group,
-        "宜万高铁": yw_group,
-        "武九客专": wj_group,
-        "武冈城际": wg_group,
-        "武孝城际": wx_group,
-        "武咸城际": wxx_group,
-        "其他信息": assistMarkersLayer
+        // "汉宜高铁": hy_group,
+        // "合武高铁": hw_group,
+        // "宜万高铁": yw_group,
+        // "武九客专": wj_group,
+        // "武冈城际": wg_group,
+        // "武孝城际": wx_group,
+        // "武咸城际": wxx_group,
+        // "其他信息": assistMarkersLayer
     };
     L.control.layers(baseLayers, overlays).addTo(map);
 }
